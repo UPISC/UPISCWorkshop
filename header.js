80a036d7b7ef61a9487ef5d9ea185bf8522a0f2d
Enrico Sarcinelli <144281172+UPISC@users.noreply.github.com>
2023-09-06 21:19:45 -0400

    Update-09-06-23

---
+++ header.js
@@ -0,0 +1,39 @@
+let appHeader = `
+	<!-- Header -->
+	<header id="header">
+
+		<nav id="nav">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+
+				<li>
+					<a href="#">Team<a>
+							<ul>
+								<li><a href="SteeringCommittee.html">Steering Committee</a></li>
+								<li><a href="ConferenceOrganizers.html">Conference Organizers</a></li>
+								<li><a href="KeyFaculty.html">Key Faculty and Researchers</a></li>
+
+							</ul>
+				</li>
+
+				<!-- <li><a href="Agenda.html">Agenda</a></li> -->
+				<li><a href="#">Agenda<a>
+					<ul>
+						<li><a href="Agenda2023.html">2023</a></li>
+						<li><a href="Agenda2022.html">2022</a></li>
+					</ul>
+				</li>
+
+				<li><a href="RelatedInitiatives.html">Related Initiatives</a></li>
+
+				<li><a href="TravelVenue.html">Travel and Venue</a></li>
+
+				<!-- <li><a href="https://pitt.co1.qualtrics.com/jfe/form/SV_eyvDLU1VyBYwr2u" -->
+				<li><a href="https://pitt.co1.qualtrics.com/jfe/form/SV_7OmbRE6JUemgWvY"
+						target="_blank">Registration</a></li>
+
+			</ul>
+		</nav>
+	</header>
+`;
+document.getElementById("app-header").innerHTML = appHeader;

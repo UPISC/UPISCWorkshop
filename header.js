006ff0630938fcadde9e13b745a4178e7b921c75
Enrico Sarcinelli <144281172+UPISC@users.noreply.github.com>
2024-08-19 11:40:22 -0400

    Update header.js

--- header.js
+++ header.js
@@ -3,32 +3,32 @@ document.write('\
 \
 			<nav id="nav">\
 				<ul>\
-					<li><a href="index.html">Home</a></li>\
+					<li><a href="https://upisc.github.io/UPISCWorkshop/index.html">Home</a></li>\
 \
 					<li>\
 						<a href="#">Team<a>\
 								<ul>\
-									<li><a href="SteeringCommittee.html">Steering Committee</a></li>\
-									<li><a href="ConferenceOrganizers.html">Conference Organizers</a></li>\
-									<li><a href="KeyFaculty.html">Key Faculty and Researchers</a></li>\
+									<li><a href="https://upisc.github.io/UPISCWorkshop/SteeringCommittee.html">Steering Committee</a></li>\
+									<li><a href="https://upisc.github.io/UPISCWorkshop/ConferenceOrganizers.html">Conference Organizers</a></li>\
+									<li><a href="https://upisc.github.io/UPISCWorkshop/KeyFaculty.html">Key Faculty and Researchers</a></li>\
 \
 								</ul>\
 					</li>\
 \
-					<li><a href="RelatedInitiatives.html">Related Initiatives</a></li>\
+					<li><a href="https://upisc.github.io/UPISCWorkshop/RelatedInitiatives.html">Related Initiatives</a></li>\
 \
-					<li><a href="TravelVenue.html">Travel and Venue</a></li>\
+					<li><a href="https://upisc.github.io/UPISCWorkshop/TravelVenue.html">Travel and Venue</a></li>\
 \
 					<li><a href="#">Previous Workshops<a>\
 						<ul>\
-							<li><a href="Agenda2024.html">2024</a></li>\
-							<li><a href="Agenda2023.html">2023</a></li>\
-							<li><a href="Agenda2022.html">2022</a></li>\
+							<li><a href="https://upisc.github.io/UPISCWorkshop/Agenda2024.html">2024</a></li>\
+							<li><a href="https://upisc.github.io/UPISCWorkshop/Agenda2023.html">2023</a></li>\
+							<li><a href="https://upisc.github.io/UPISCWorkshop/Agenda2022.html">2022</a></li>\
 						</ul>\
 					</li>\
 \
 					<li style="white-space: nowrap;">\
-						<a href="Agenda2024.html" class="button primary">UPISC Workshop</a>\
+						<a href="https://upisc.github.io/UPISCWorkshop/Agenda2024.html" class="button primary">UPISC Workshop</a>\
 					</li>\
 \
 					<li style="white-space: nowrap;">\
